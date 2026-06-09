bfa29c1705463dc50b1700ad88d483d8ab7ff2bb
Aniket Patil <114986841+Aniket8195@users.noreply.github.com>
2026-06-09 23:52:08 +0530

    Update 344. Reverse String.java

--- Two Pointer/344. Reverse String.java
+++ Two Pointer/344. Reverse String.java
@@ -1,24 +1,16 @@
 class Solution {
-    public boolean isPalindrome(String s) {
+    public void reverseString(char[] s) {
+         int l=0;
+         int r=s.length-1;
 
-        int n=s.length();
-        if(s.isEmpty()){
-             return true;
-        }
-        int l=0;
-        int r=n-1;
+         while(l<=r){
+            char temp=s[l];
 
-        while(l<=r){
-         char lc=s.charAt(l);
-         char rc=s.charAt(r);
-            if(!Character.isLetterOrDigit(lc)) l++;
-            else if(!Character.isLetterOrDigit(rc)) r--;
-            else{
-                if(Character.toLowerCase(lc)!=Character.toLowerCase(rc))return false;
-                l++;
-                r--;
-            }
-        }
-        return true;
+           s[l]=s[r];
+           s[r]=temp;
+           l++;
+           r--;
+         }
+
     }
 }
